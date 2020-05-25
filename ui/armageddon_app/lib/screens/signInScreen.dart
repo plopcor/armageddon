277b3b8cf7e6a174789e291c9e872787d4f7a27b
alejandro <alejotronics@gmail.com>
2020-05-25 20:33:43 +0200

    test login and logout

--- ui/armageddon_app/lib/screens/signInScreen.dart
+++ ui/armageddon_app/lib/screens/signInScreen.dart
@@ -30,6 +30,12 @@ class SignInScreen extends StatelessWidget {
               height: 150,
             ),
             SingInForm(),
+            RaisedButton(
+              onPressed: () {
+                logout().then((value) => log(value.toString()));
+              },
+              child: Text('Log out'),
+            ),
           ],
         ),
       ),
--- ui/armageddon_app/lib/services/authenticationServices.dart
+++ ui/armageddon_app/lib/services/authenticationServices.dart
@@ -16,16 +16,49 @@ Future<User> login({String password, String username}) async {
       .post(url, body: {'usuario': '$username', 'contraseña': '$password'});
 
   User user;
-  String token;
   if (response.statusCode == 200) {
     Map<String, dynamic> result = jsonDecode(response.body);
     result = result['data'];
+
     user = User.fromJson(result['usuario']);
-    token = result['token'].toString();
-    log(user.toString());
-  } else {
-    user = null;
+
+    String token = result['token'].toString();
+    _setToken(token);
   }
 
   return user;
+}
+
+/// Logout - Send token throught GET
+Future<bool> logout() async {
+  final url = '$apiUrl/logout';
+
+  final token = await _getToken();
+
+  final response = await http.get(url,
+      headers: {'Accept': 'application/json', 'Authorization': "$token"});
+
+  log(jsonDecode(response.body).toString());
+
+  if (response.statusCode == 200) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+
+/// Returns the token from Shared Preferences
+Future<String> _getToken() async {
+  final SharedPreferences prefs = await _prefs;
+
+  return prefs.getString('token') ?? 'NO TOKEN';
+}
+
+/// Saves the token in Shared Preferences
+Future<bool> _setToken(String token) async {
+  final SharedPreferences prefs = await _prefs;
+
+  return prefs.setString('token', token);
 }
