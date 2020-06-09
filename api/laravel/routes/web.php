417891b6f08f9ff6dd333ae95362762268cc1f86
Pol <pollopcor@gmail.com>
2020-06-09 03:00:12 +0200

    [Web] Rutas - Creado rutas para la interfaz web

--- api/laravel/routes/web.php
+++ api/laravel/routes/web.php
@@ -11,12 +11,50 @@
 |
 */
 
-
+/**
+ * PANEL principal
+ */
 Route::get('/', function () {
-    return view('welcome');
+    if(Auth::check()) {
+        return view('panel');
+    } else {
+        return redirect('login');
+    }
 });
-//
-//
-//Auth::routes();
-//
+
+Auth::routes();
+
+/**
+ * TIENDA PROPIA
+ */
+Route::group(['prefix' => 'tienda', 'middleware' => 'auth'], function () {
+
+    // Tienda
+    Route::get('/', 'WebControllers\TiendaPropiaController@ver')->name('tienda');
+
+    // Productos
+    Route::get('/productos', 'WebControllers\TiendaPropiaController@productos')->name('productos');
+
+    // Horario
+    Route::get('/horario', 'WebControllers\TiendaPropiaController@horario')->name('horario');
+
+    // Pedidos
+    Route::get('/pedidos', 'WebControllers\TiendaPropiaController@productos')->name('pedidos');
+
+    // Clientes
+    Route::get('/clientes', 'WebControllers\TiendaPropiaController@clientes')->name('clientes');
+
+    // Estadisticas
+    Route::get('/estadisticas', 'WebControllers\TiendaPropiaController@estadisticas')->name('estadisticas');
+
+});
+
+/**
+ * USUARIO
+ */
+Route::group(['prefix' => 'usuario'], function () {
+
+    // Perfil
+    Route::get('/', 'WebController\UsuarioController@perfil')->name('usuario');
+});
 
